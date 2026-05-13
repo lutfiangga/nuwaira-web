8222fe1ab5376420cbbc662b40aae031881af4c2
lutfiangga <lutfiangga2908@gmail.com>
2026-05-13 11:52:41 +0700

    update meta tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,9 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
 
 	let { children } = $props();
 </script>
 
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
 <main class="bg-white">
 	{@render children()}
 </main>
